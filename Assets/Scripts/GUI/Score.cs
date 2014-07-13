5dbad925ce6e1a7e9921853c750ccaf279271e10
Thomaz Padilha <padilha.thomaz@gmail.com>
2014-07-12 23:23:39 -0300

    Correção do script score

--- Assets/Scripts/GUI/Score.cs
+++ Assets/Scripts/GUI/Score.cs
@@ -2,20 +2,33 @@
 using System.Collections;
 
 public class Score : MonoBehaviour {
-
+	/// <summary>
+	/// Campo privado para armazenar o placar real
+	/// </summary>
 	private static int score;
 
+	/// <summary>
+	/// Metodo acessador que retorna o placar atual
+	/// </summary>
+	/// <value>O placar atual.</value>
 	public static int CurrentScore{
 		get {
 			return score;
 		}
 	}
 
+	/// <summary>
+	/// Metodo para acrescentar pontos ao placar
+	/// </summary>
+	/// <param name="scoreAmount">Quantidade de pontos a ser acrescentada.</param>
 	public static void AddScore(int scoreAmount){
 		if(scoreAmount > 0)
 			score += scoreAmount;
 	}
 
+	/// <summary>
+	/// Reseta o placar
+	/// </summary>
 	public static void ResetScore(){
 		score = 0;
 	}
